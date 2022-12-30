350a12ab50f27f3f9d43bc62a6fc311409424209
PratyayChakraborty <pratyaychakraborty024@gmail.com>
2022-12-30 11:59:32 +0530

    added all entity

--- Mini-project/src/main/java/com/ecom/MiniProjectApplication.java
+++ Mini-project/src/main/java/com/ecom/MiniProjectApplication.java
@@ -1,13 +1,23 @@
 package com.ecom;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MiniProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniProjectApplication.class, args);
 	}
 
+	@Bean
+	ModelMapper modelMapper () {
+		
+		return new ModelMapper();
+	}
 }
